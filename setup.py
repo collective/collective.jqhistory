c024a910cfcd2b11882ea185d7b88d311ce616c5
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-18 10:10:00 +0000

    egg cleanup

--- setup.py
+++ setup.py
@@ -1,32 +1,36 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = open('collective/jqhistory/version.txt').read().strip()
+maintainer = 'Victor Baumann'
 
 setup(name='collective.jqhistory',
       version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="" + \
+          ' (Maintainer %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
-      author='Victor BAUMANN',
-      author_email='v.baumann@4teamwork.ch',
-      url='',
-      license='GPL',
+      keywords='jquery history',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/ftw/collective.jqhistory/',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = opengever
       """,
       )
