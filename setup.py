2210063b42e2f1b2cdb24e214a84a685299536f9
Jonas Baumann <jone@jone.ch>
2011-11-30 13:29:12 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -2,35 +2,43 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.0dev'
-maintainer = 'Victor Baumann'
 
 setup(name='collective.jqhistory',
       version=version,
-      description="" + \
-          ' (Maintainer %s)' % maintainer,
-      long_description=open("README.rst").read() + "\n" + \
-          open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description='Packages up the jquery-history plugin for plone.',
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'License :: OSI Approved :: GNU Affero General Public License v3'
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-      keywords='jquery history',
-      author='%s, 4teamwork GmbH' % maintainer,
+
+      keywords='jquery history collective plone',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
-      url='http://psc.4teamwork.ch/oss/collective/collective.jqhistory/',
+      url='https://github.com/collective/collective.jqhistory/',
       license='AGPL3',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
         ],
-      entry_points="""
+
+      entry_points='''
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
-      target = collective
-      """,
+      target = plone
+      ''',
       )
